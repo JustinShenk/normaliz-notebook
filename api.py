d027cc688ce18bcdf790a4ae26136e1277c1db9d
Alexander Höreth <ahoereth@gmail.com>
2017-01-27 16:13:26 +0100

    Add CORS and comments.

--- api.py
+++ api.py
@@ -1,10 +1,12 @@
 #! /usr/bin/env python3
 import json
-
 from flask import Flask, request, abort, redirect
+from flask_cors import CORS
 
 
+PORT = 8090
 app = Flask(__name__)
+CORS(app)
 
 
 def tofloat(val):
@@ -20,10 +22,14 @@ def normaliz(v1, v2):
     """Get the hilbert basis for the two passed vectors."""
     v1 = list(map(tofloat, v1.split(',')))[:2]
     v2 = list(map(tofloat, v2.split(',')))[:2]
+
+    # TODO: Calculate Hilber Basis
     # cone = PyNormaliz.NmzCone('cone', [v1, v2])
     # hb = PyNormaliz.NmzResult(cone, 'HilbertBasis')
+
+    # TODO: Return Hilbert Basis for visualization on client side.
     return json.dumps([v1, v2])
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    app.run(port=PORT)
