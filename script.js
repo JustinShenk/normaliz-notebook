fee5aab5949ce47326370d87e80c6c2a23dc318d
Justin Shenk <shenkjustin@gmail.com>
2017-02-01 22:54:01 +0100

    Restrict vectors to integrals  - Create lookup dictionary for all combinations 0-10 for x and y of two vectors  - [WIP] Provide access to Hilbert basis lookup automatically upon dropping vector pin

--- script.js
+++ script.js
@@ -1,26 +1,8 @@
-var a2 = Math.atan2(source.y, source.x);
-var a1 = Math.atan2(compare.y, compare.x);
-var sign = a1 > a2 ? 1 : -1;
-var angle = a1 - a2;
-var K = -sign * Math.PI * 2;
-var angle = (Math.abs(K + angle) < Math.abs(angle))? K + angle : angle;
+// Load Hilbert Basis lookup dictionary.
 
-var jsonCircles = [
-  { "x_axis": 30, "y_axis": 30, "radius": 20, "color" : "green" },
-  { "x_axis": 70, "y_axis": 70, "radius": 20, "color" : "purple"},
-  { "x_axis": 110, "y_axis": 100, "radius": 20, "color" : "red"}];
+var hilbertBases = {};
 
-var svgContainer = d3.select("body").append("svg")
-                                    .attr("width", 200)
-                                    .attr("height", 200);
-
-var circles = svgContainer.selectAll("circle")
-                          .data(jsonCircles)
-                          .enter()
-                          .append("circle");
-
-var circleAttributes = circles
-                       .attr("cx", function (d) { return d.x_axis; })
-                       .attr("cy", function (d) { return d.y_axis; })
-                       .attr("r", function (d) { return d.radius; })
-                       .style("fill", function(d) { return d.color; });
+$.getJSON('bases.json',function(data) {
+  hilbertBases = data
+  console.log('Hilbert bases loaded (test): ' + hilbertBases['[3, 3],[1, 6]'])
+});
