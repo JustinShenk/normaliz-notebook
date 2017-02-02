d56e41e1b00d861b4595eb40e1f35f02a8ed1fba
Justin Shenk <shenkjustin@gmail.com>
2017-02-02 15:08:57 +0100

    Add output of Hilbert Basis for vector selection

--- script.js
+++ script.js
@@ -1,8 +1,36 @@
 // Load Hilbert Basis lookup dictionary.
+console.log("src loaded");
 
-var hilbertBases = {};
+var hilbertBases = {}
+$(document).ready(function() { 
+	console.log("script loaded");
 
-$.getJSON('bases.json',function(data) {
-  hilbertBases = data
-  console.log('Hilbert bases loaded (test): ' + hilbertBases['[3, 3],[1, 6]'])
+	$.getJSON('bases.json',function(data) {
+	  hilbertBases = data;
+	});
 });
+
+function calculate() {
+    var sourceEnc = escape($("#sourceVector").val());
+    var compareEnc = escape($("#compareVector").val());
+    var [sx, sy] = sourceEnc.split('%2C');
+    var [cx, cy] = compareEnc.split('%2C');
+    var vectorCombination = '[' + String(sx) + ', ' + String(sy) + '],[' + String(cx) + ', ' + String(cy) + ']';
+    var output = JSON.stringify(hilbertBases[vectorCombination]);
+    // console.log(String(output));
+    $("#output").html(output);
+    drawHilbertBasis(hilbertBases[vectorCombination]);                
+};
+
+function drawHilbertBasis(basis) {
+	basis.forEach(function(element) {		
+		basisCircles.append("circle")
+		.attr("r", 4)
+		.attr("fill", "yellow")
+		.attr("fill-opacity", 0.8)
+		.attr("cx", element[0] * scaleFactor)
+		.attr("cy", element[1] * -scaleFactor);
+	});
+	console.log(JSON.stringify(basis));
+	
+}
