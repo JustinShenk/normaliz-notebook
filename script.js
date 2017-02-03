5c065301f8ea4919e6c4f5fd19ec97f7e65eaaa3
Justin Shenk <shenkjustin@gmail.com>
2017-02-03 21:11:30 +0100

    Visualize Hilbert basis with yellow circles - Prettify bases.json - Restrict window size

--- script.js
+++ script.js
@@ -2,35 +2,43 @@
 console.log("src loaded");
 
 var hilbertBases = {}
-$(document).ready(function() { 
-	console.log("script loaded");
+    // $(document).ready(function() { 
+console.log("script loaded");
 
-	$.getJSON('bases.json',function(data) {
-	  hilbertBases = data;
-	});
+$.getJSON('bases.json', function(data) {
+    hilbertBases = data;
 });
+// });
 
 function calculate() {
-    var sourceEnc = escape($("#sourceVector").val());
-    var compareEnc = escape($("#compareVector").val());
+    var sourceEnc = escape($("#sourceInput").val());
+    var compareEnc = escape($("#compareInput").val());
     var [sx, sy] = sourceEnc.split('%2C');
     var [cx, cy] = compareEnc.split('%2C');
     var vectorCombination = '[' + String(sx) + ', ' + String(sy) + '],[' + String(cx) + ', ' + String(cy) + ']';
+    console.log(vectorCombination)
     var output = JSON.stringify(hilbertBases[vectorCombination]);
-    // console.log(String(output));
     $("#output").html(output);
-    drawHilbertBasis(hilbertBases[vectorCombination]);                
+    drawHilbertBasis(hilbertBases[vectorCombination]);
 };
 
 function drawHilbertBasis(basis) {
-	basis.forEach(function(element) {		
-		basisCircles.append("circle")
-		.attr("r", 4)
-		.attr("fill", "yellow")
-		.attr("fill-opacity", 0.8)
-		.attr("cx", element[0] * scaleFactor)
-		.attr("cy", element[1] * -scaleFactor);
-	});
-	console.log(JSON.stringify(basis));
-	
+    console.log(basis);
+    svg.selectAll("circle.basisCircles").remove();
+    if (basis != undefined) {
+        basis.forEach(function(element) {
+            basisCircles.append("circle")
+                .attr("class", "basisCircles")
+                .attr("r", 4)
+                .attr("fill", "yellow")
+                .attr("fill-opacity", 0.8)
+                .attr("cx", element[0] * scaleFactor)
+                .attr("cy", element[1] * -scaleFactor);
+        });
+        console.log(JSON.stringify(basis));
+    } else {
+    	console.log("Error: Basis not loaded")
+    }
+    update();
+
 }
