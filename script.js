020cde29615b2e24b039677158b047c4d06c6b12
Justin Shenk <shenkjustin@gmail.com>
2017-02-06 13:12:14 +0100

    [WIP] Develop interface and design

--- script.js
+++ script.js
@@ -1,34 +1,16 @@
 // Load Hilbert Basis lookup dictionary.
-console.log("src loaded");
-
 var hilbertBases = {}
-    // $(document).ready(function() { 
-console.log("script loaded");
 
-$.getJSON('bases.json', function(data) {
-    hilbertBases = data;
+$(document).ready(function() {
+    $.getJSON('bases.json', function(data) {
+        console.log('bases loaded');
+        hilbertBases = data;
+        calculate();
+    })
 });
-// });
-
-function calculate() {
-    var sourceEnc = escape($("#sourceInput").val());
-    var compareEnc = escape($("#compareInput").val());
-    var [sx, sy] = sourceEnc.split('%2C');
-    var [cx, cy] = compareEnc.split('%2C');
-    var vectorCombination = '[' + String(sx) + ', ' + String(sy) + '],[' + String(cx) + ', ' + String(cy) + ']';
-    console.log(vectorCombination)
-    var output = JSON.stringify(hilbertBases[vectorCombination]);
-    $("#output").html(output);
-    sourceVector.datum().x = sx * scaleFactor;
-    sourceVector.datum().y = -sy * scaleFactor;
-    compareVector.datum().x = cx * scaleFactor;
-    compareVector.datum().y = -cy * scaleFactor;
-    drawHilbertBasis(hilbertBases[vectorCombination]);
-};
 
 function drawHilbertBasis(basis) {
-    console.log(basis);
-    svg.selectAll("circle.basisCircles").remove();
+    svgContainer.selectAll("circle.basisCircles").remove();
     if (basis != undefined) {
         basis.forEach(function(element) {
             basisCircles.append("circle")
@@ -43,5 +25,25 @@ function drawHilbertBasis(basis) {
         console.log("Error: Basis not loaded")
     }
     update();
-
 }
+
+function calculate() {
+    if (hilbertBases == undefined) {
+        $.getJSON('bases.json', function(data) {
+            console.log('bases loaded');
+            hilbertBases = data;
+        })
+    }
+    var sourceEnc = escape($("#sourceInput").val());
+    var compareEnc = escape($("#compareInput").val());
+    var [sx, sy] = sourceEnc.split('%2C');
+    var [cx, cy] = compareEnc.split('%2C');
+    var vectorCombination = '[' + String(sx) + ', ' + String(sy) + '],[' + String(cx) + ', ' + String(cy) + ']';
+    var output = JSON.stringify(hilbertBases[vectorCombination]);
+    $("#output").html(output);
+    sourceVector.datum().x = sx * scaleFactor;
+    sourceVector.datum().y = -sy * scaleFactor;
+    compareVector.datum().x = cx * scaleFactor;
+    compareVector.datum().y = -cy * scaleFactor;
+    drawHilbertBasis(hilbertBases[vectorCombination]);
+};
